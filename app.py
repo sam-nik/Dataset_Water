597f6fbf0161dbf7ce773c9cf1cbac4ad9b936ec
Shubham Nikam <89242708+sam-nik@users.noreply.github.com>
2023-03-01 21:03:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,33 @@
+
+import streamlit as st
+import numpy as np
+from skimage.io import imread
+from skimage.transform import resize
+import pickle
+from PIL import Image
+st.title('water quality assiment by image classification')
+st.text('Upload image')
+
+model = pickle.load(open('img_model.p','rb'))
+
+uploaded_file = st.file_uploader("Select image...", type="jpg")
+if uploaded_file is not None:
+  img = Image.open(uploaded_file)
+  st.image(img,caption='Uploaded Image')
+
+  if st.button('predict'):
+    CATEGORIES = ['muddy river water','clean river water','trash river water']
+    st.write('output')
+  flat_data=[]
+  img = np.array(img)
+  
+  img_resized = resize(img,(150,150,3))
+  flat_data.append(img_resized.flatten())
+  flat_data = np.array(flat_data)
+  y_out = model.predict(flat_data)
+  y_out = CATEGORIES [y_out[0]]
+  st.title(f'PREDICTED OUTPUT: {y_out}')
+
+q = model.predict_proba(flat_data)
+for index, item in enumerate(CATEGORIES):
+  st.write(f'{item}:{q[0][index]*100}%')
